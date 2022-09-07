db9c3362d2c7551a8c76b7cd66561be2e7e580d0
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-09-07 12:06:46 +1000

    getting form data completed

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
+const initialState = {
+  email: "",
+  password: "",
+};
+
 export const Login = () => {
+  const [formDt, setFormDt] = useState(initialState);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormDt({ ...formDt, [name]: value });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(formDt);
+  };
+
   return (
     <Container>
       <Row>
         <Col>
           <h3 className="text-center">Client Login</h3>
           <hr />
-          <Form>
+          <Form onSubmit={handleOnSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
+                onChange={handleOnChange}
                 name="email"
                 placeholder="Enter email"
                 required
@@ -23,6 +41,7 @@ export const Login = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
+                onChange={handleOnChange}
                 name="password"
                 placeholder="Password"
                 required
